f15c53b0565623a8cb143914e5bd3a512eedbe4b
fayeah <411596701@qq.com>
2020-09-15 17:05:13 +0800

    简化对data的赋值

--- src/components/toast/src/main.js
+++ src/components/toast/src/main.js
@@ -5,9 +5,8 @@ const ToastConstructor = Vue.extend(Main)
 
 let instance
 const toast = function (options) {
-  options = options || {}
   instance = new ToastConstructor({
-    data: options
+    data: options || {}
   })
   instance.vm = instance.$mount()
   document.body.appendChild(instance.vm.$el)
